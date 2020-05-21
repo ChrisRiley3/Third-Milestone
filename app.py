96fffd6ad702362172f98a685cc1c62818495f2f
Christopher Riley <chris_riley1993@hotmail.com>
2020-05-21 15:08:14 +0000

    added update function on app.py

--- app.py
+++ app.py
@@ -62,6 +62,27 @@ def insert_game():
     return redirect(url_for('suggested'))
 
 
+@app.route('/edit_game/<game_id>')
+def edit_game(game_id):
+    the_game = mongo.db.suggested.find_one({"_id": ObjectId(game_id)})
+    all_genres = mongo.db.genre.find()
+    return render_template('edit_game.html', game=the_game,
+                           genre=all_genres)
+
+
+@app.route('/update_game/<game_id>', methods=["POST"])
+def update_game(game_id):
+    suggested = mongo.db.suggested
+    suggested.update({'_id': ObjectId(game_id)},
+                     {
+                     'first_name': request.form.get('first_name'),
+                     'last_name': request.form.get('last_name'),
+                     'game_title': request.form.get('game_title'),
+                     'game_description': request.form.get('game_description')
+                     })
+    return redirect(url_for('suggested'))
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
