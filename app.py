d29ba2846e64523ee68b5125069ea383a9f8f67f
Christopher Riley <chris_riley1993@hotmail.com>
2020-05-21 16:09:55 +0000

    added commenting to the files to make it easy readable

--- app.py
+++ app.py
@@ -13,55 +13,55 @@ app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
 mongo = PyMongo(app)
 
-
+# This will render the index page first
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
+# This will go through my mongodb and find the ps4 data then display it on the ps4.html page
 @app.route('/ps4')
 def ps4():
     return render_template('ps4.html',
                            playstation4=mongo.db.playstation4.find())
 
-
+# This will go through my mongodb and find the xbox data then display it on the xbox.html page
 @app.route('/xbox')
 def xbox():
     return render_template('xbox.html',
                            xbox=mongo.db.xbox.find())
 
-
+# This will go through my mongodb and find the nintendo data then display it on the nintendo.html page
 @app.route('/nintendo')
 def nintendo():
     return render_template('nintendo.html',
                            nintendo=mongo.db.nintendo.find())
 
-
+# This will go through my mongodb and find the pc data then display it on the pc.html page
 @app.route('/pc')
 def pc():
     return render_template('pc.html',
                            pc=mongo.db.pc.find())
 
-
+# This will go through my mongodb and find the suggested data then display it on the suggested.html page
 @app.route('/suggested')
 def suggested():
     return render_template('suggested.html',
                            suggested=mongo.db.suggested.find())
 
-
+# This will go through my mongodb and find the genre data then display it on the add_game.html page
 @app.route('/add_game')
 def add_game():
     return render_template('add_game.html',
                            genre=mongo.db.genre.find())
 
-
+# This will allow the user to insert a game suggestion when they click the submit button
 @app.route('/insert_game', methods=['POST'])
 def insert_game():
     suggested = mongo.db.suggested
     suggested.insert_one(request.form.to_dict())
     return redirect(url_for('suggested'))
 
-
+# This will allow the user to edit a game suggestion
 @app.route('/edit_game/<game_id>')
 def edit_game(game_id):
     the_game = mongo.db.suggested.find_one({"_id": ObjectId(game_id)})
@@ -69,7 +69,7 @@ def edit_game(game_id):
     return render_template('edit_game.html', game=the_game,
                            genre=all_genres)
 
-
+# This will allow the previous suggestion to be edited and not post a new suggestion
 @app.route('/update_game/<game_id>', methods=["POST"])
 def update_game(game_id):
     suggested = mongo.db.suggested
@@ -80,6 +80,12 @@ def update_game(game_id):
                      'game_title': request.form.get('game_title'),
                      'game_description': request.form.get('game_description')
                      })
+    return redirect(url_for('suggested'))
+
+# This will allow the user to delete their suggestion
+@app.route('/delete_game/<game_id>')
+def delete_game(game_id):
+    mongo.db.suggested.remove({'_id': ObjectId(game_id)})
     return redirect(url_for('suggested'))
 
 
